96562380065701dc3c09c08b6b52efeb28491ae4
Tambass <tambass85@gmail.com>
2020-07-16 10:08:09 +0200

    ajout nav-bar dans main.hbs

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const exphbs = require('express-handlebars')
-const Handlebars = require('handlebars')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const Handlebars = require("handlebars");
 const {
-    allowInsecurePrototypeAccess,
-  } = require("@handlebars/allow-prototype-access");
-  const methodOverride = require("method-override");
-  const path = require("path");
-  const sharp = require("sharp");
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const methodOverride = require("method-override");
+const path = require("path");
+const sharp = require("sharp");
 
-  
+// Express
+const port = 8080;
+const app = express();
+
+//Method Override
+app.use(methodOverride("_method"));
+
+// Handlebars
+
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: "hbs",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
+
+app.set("view engine", "hbs");
+
+// BodyParser
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(port, function () {
+  console.log(
+    `écoute le port ${port}, lancé à : ${new Date().toLocaleString()}`
+  );
+});
